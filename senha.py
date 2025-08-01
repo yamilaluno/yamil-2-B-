b1e919fc9c663fb5ff126249174445b06ceb3082
yamilaluno <alanocaflores8@gmail.com>
2025-08-01 13:07:30 -0300

    Create senha.py

---
+++ senha.py
@@ -0,0 +1,11 @@
+# Senha correta
+senha_correta = "1234"
+
+# Entrada do usuário
+senha_digitada = input("Digite a senha: ")
+
+# Verificação
+if senha_digitada == senha_correta:
+    print("Acesso permitido!")
+else:
+    print("Senha incorreta!")
