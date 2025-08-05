2a4442803d693746219642704c62a37d591b17c1
yamilaluno <alanocaflores8@gmail.com>
2025-08-05 09:11:57 -0300

    Create meta smart.py

---
+++ meta smart.py
@@ -0,0 +1,12 @@
+print("Vamos montar sua meta SMART!")
+
+especifica = input("1. O que exatamente você quer alcançar? (S - Especifica): ")
+mensuravel = input("2. Como saberemos se você atingiu a meta? (M- Mensurável):")
+atingivel = input("3. Essa meta é possível de realizar? Como? (A - Atingivel): ")
+relevante = input("4. Por que essa meta é importante para você? (R - Relevante): ")
+temporal = input("5. Em quanto tempo você quer alcançar essa meta? (T - Temporal): ")
+
+print("\n Sua meta SMART e:\n")
+print(f"Eu quero {especifica}, e saberei ui quando {mensuravel}.")
+print(f"Essa meta é possível porque {atingivel}. Ela é importante pois {relevante}.")
+print(f"Quero alcançar isso em {temporal}.")
