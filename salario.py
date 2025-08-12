bb5125cf5d633f27d2b5c6de7813ab652b15f70c
yamilaluno <alanocaflores8@gmail.com>
2025-08-12 08:33:56 -0300

    Create salario.py

---
+++ salario.py
@@ -0,0 +1,12 @@
+# Entrada
+salario = float(input("Digite o salário: R$ "))
+
+# Processamento
+essenciais = salario * 0.50
+prioridades = salario * 0.15
+estilo = salario * 0.35
+
+# Saída
+print(f"Essenciais: R$ {essenciais:.2f}")
+print(f"Prioridades: R$ {prioridades:.2f}")
+print(f"Estilo de vida: R$ {estilo:.2f}")
