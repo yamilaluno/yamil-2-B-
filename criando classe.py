6613ccbc7f859c1dd4d32245ed512b938c537b48
yamilaluno <alanocaflores8@gmail.com>
2025-08-25 09:58:18 -0300

    Create criando classe.py

---
+++ criando classe.py
@@ -0,0 +1,35 @@
+class pessoa:
+    nome = ""
+    idade = 0
+
+# Primeira Pessoa
+p1 = pessoa()
+p1.nome = "Matheus Borges"
+p1.idade = 17
+
+# Segunda Pessoa
+p2 = pessoa()
+p2.nome = "Matheus Morais"
+p2.idade = 16
+
+# Terceira Pessoa
+p3 = pessoa()
+p3.nome = "Rodrigo Raimundo"
+p3.idade = 16
+
+# Quarta Pessoa
+p4 = pessoa()
+p4.nome = "Diego alves"
+p4.idade = 18
+
+# Quinta Pessoa
+p5 = pessoa()
+p5.nome = "Matheus Pizzo"
+p5.idade = 17
+
+# Imprimindo Dados
+print("Pessoa 1:", p1.nome, "-", p1.idade, " anos")
+print("Pessoa 2:", p2.nome, "-", p2.idade, " anos")
+print("Pessoa 3:", p3.nome, "-", p3.idade, " anos")
+print("Pessoa 4:", p4.nome, "-", p4.idade, " anos")
+print("Pessoa 5:", p5.nome, "-", p5.idade, " anos")
