1557a521f81392234566e70d946e3b5a9fbcc1d7
yamilaluno <alanocaflores8@gmail.com>
2025-08-29 13:50:18 -0300

    Add files via upload

---
+++ classe de carros.py
@@ -0,0 +1,64 @@
+class Carro:
+    marca = ""
+    modelo = ""
+    ano = ""
+
+
+# Primeiro carro
+p1 = Carro()
+p1.marca = "Toyota"
+p1.modelo = "Corolla"
+p1.ano = "2020"
+
+# Segundo carro
+p2 = Carro()
+p2.marca = "Honda"
+p2.modelo = "Civic"
+p2.ano = "2019"
+
+# Terceiro carro
+p3 = Carro()
+p3.marca = "Ford"
+p3.modelo = "Mustang"
+p3.ano = "2021"
+
+# Quarto carro
+p4 = Carro()
+p4.marca = "Chevrolet"
+p4.modelo = "Onix"
+p4.ano = "2018"
+
+# Quinto carro
+p5 = Carro()
+p5.marca = "Volkswagen"
+p5.modelo = "Golf"
+p5.ano = "2017"
+
+# Sexto carro
+p6 = Carro()
+p6.marca = "Fiat"
+p6.modelo = "Argo"
+p6.ano = "2022"
+
+# Sétimo carro
+p7 = Carro()
+p7.marca = "Hyundai"
+p7.modelo = "HB20"
+p7.ano = "2021"
+
+# Oitavo carro
+p8 = Carro()
+p8.marca = "Jeep"
+p8.modelo = "Compass"
+p8.ano = "2020"
+
+
+# Imprimindo Dados
+print("Carro 1:", p1.marca, "-", p1.modelo, "-", p1.ano)
+print("Carro 2:", p2.marca, "-", p2.modelo, "-", p2.ano)
+print("Carro 3:", p3.marca, "-", p3.modelo, "-", p3.ano)
+print("Carro 4:", p4.marca, "-", p4.modelo, "-", p4.ano)
+print("Carro 5:", p5.marca, "-", p5.modelo, "-", p5.ano)
+print("Carro 6:", p6.marca, "-", p6.modelo, "-", p6.ano)
+print("Carro 7:", p7.marca, "-", p7.modelo, "-", p7.ano)
+print("Carro 8:", p8.marca, "-", p8.modelo, "-", p8.ano)
