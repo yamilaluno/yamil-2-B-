a71f69245d24e1c11fcd3f562c8cf0a1023ef075
yamilaluno <alanocaflores8@gmail.com>
2025-08-29 13:36:44 -0300

    Add files via upload

---
+++ classe de filma.py
@@ -0,0 +1,64 @@
+class filme:
+    titulo = ""
+    ano = ""
+    genero = ""
+    classificação = ""
+
+
+# Primeiro filme
+p1 = filme()
+p1.titulo = "Minecreft o Filme"
+p1.ano = "2025"
+p1.genero = "Aventura/Comédia"
+p1.classificação = "10"
+
+# Segundo filme
+p2 = filme()
+p2.titulo = "Sonic 3 - O Filme"
+p2.ano = "2024"
+p2.genero = "Ação/Comédia"
+p2.classificação = "10"
+
+# Terceiro filme
+p3 = filme()
+p3.titulo = "It - A Coisa"
+p3.ano = "2017"
+p3.genero = "Terror/Mistério"
+p3.classificação = "16"
+
+# Quarto filme
+p4 = filme()
+p4.titulo = "Angry Birds - O Filme"
+p4.ano = "2016"
+p4.genero = "Infantil/Comédia"
+p4.classificação = "Livre"
+
+# Quinto filme
+p5 = filme()
+p5.titulo = "Pânico 6"
+p5.ano = "2023"
+p5.genero = "Terror/Mistério"
+p5.classificação = "18"
+
+# Sexto filme
+p6 = filme()
+p6.titulo = "Deadpool"
+p6.ano = "2016"
+p6.genero = "Ação/Comédia"
+p6.classificação = "16"
+
+# Setimo filme
+p7 = filme()
+p7.titulo = "Pokémon: Detetive Pikachu"
+p7.ano = "2019"
+p7.genero = "Infantil/Fantasia"
+p7.classificação = "Livre"
+
+# Imprimindo Dados
+print("Filme 1:", p1.titulo, "-", p1.ano, "- Gênero:", p1.genero, "- Classificação:", p1.classificação)
+print("Filme 2:", p2.titulo, "-", p2.ano, "- Gênero:", p2.genero, "- Classificação:", p2.classificação)
+print("Filme 3:", p3.titulo, "-", p3.ano, "- Gênero:", p3.genero, "- Classificação:", p3.classificação)
+print("Filme 4:", p4.titulo, "-", p4.ano, "- Gênero:", p4.genero, "- Classificação:", p4.classificação)
+print("Filme 5:", p5.titulo, "-", p5.ano, "- Gênero:", p5.genero, "- Classificação:", p5.classificação)
+print("Filme 6:", p6.titulo, "-", p6.ano, "- Gênero:", p6.genero, "- Classificação:", p6.classificação)
+print("Filme 7:", p7.titulo, "-", p7.ano, "- Gênero:", p7.genero, "- Classificação:", p7.classificação)
