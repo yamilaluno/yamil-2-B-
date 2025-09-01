ae49ffd8f6e04f8e972e9a79e1157478ab4cf64f
yamilaluno <alanocaflores8@gmail.com>
2025-09-01 09:49:23 -0300

    Add files via upload

---
+++ classe de animais.py
@@ -0,0 +1,78 @@
+class Animal:
+    nome = ""
+    especie = ""
+    idade = ""
+
+
+# Primeiro animal
+p1 = Animal()
+p1.nome = "toddyn"
+p1.especie = "tucano"
+p1.idade = "5 anos"
+
+# Segundo animal
+p2 = Animal()
+p2.nome = "rex"
+p2.especie = "cachorro"
+p2.idade = "3 anos"
+
+# Terceiro animal
+p3 = Animal()
+p3.nome = "mel"
+p3.especie = "gato"
+p3.idade = "2 anos"
+
+# Quarto animal
+p4 = Animal()
+p4.nome = "beto"
+p4.especie = "papagaio"
+p4.idade = "1 ano"
+
+# Quinto animal
+p5 = Animal()
+p5.nome = "robison"
+p5.especie = "rato"
+p5.idade = "1 ano"
+
+# Sexto animal
+p6 = Animal()
+p6.nome = "robin"
+p6.especie = "cavalo"
+p6.idade = "3 anos"
+
+# Sétimo animal
+p7 = Animal()
+p7.nome = "nemo"
+p7.especie = "peixe-palhaço"
+p7.idade = "1 ano"
+
+# Oitavo animal
+p8 = Animal()
+p8.nome = "dori"
+p8.especie = "coelho"
+p8.idade = "2 anos"
+
+# Nono animal
+p9 = Animal()
+p9.nome = "bob"
+p9.especie = "burro"
+p9.idade = "4 anos"
+
+# Décimo animal
+p10 = Animal()
+p10.nome = "simba"
+p10.especie = "leão"
+p10.idade = "3 anos"
+
+
+# Imprimindo os animais
+print("Animal 1:", p1.nome, "-", p1.especie, "-", p1.idade)
+print("Animal 2:", p2.nome, "-", p2.especie, "-", p2.idade)
+print("Animal 3:", p3.nome, "-", p3.especie, "-", p3.idade)
+print("Animal 4:", p4.nome, "-", p4.especie, "-", p4.idade)
+print("Animal 5:", p5.nome, "-", p5.especie, "-", p5.idade)
+print("Animal 6:", p6.nome, "-", p6.especie, "-", p6.idade)
+print("Animal 7:", p7.nome, "-", p7.especie, "-", p7.idade)
+print("Animal 8:", p8.nome, "-", p8.especie, "-", p8.idade)
+print("Animal 9:", p9.nome, "-", p9.especie, "-", p9.idade)
+print("Animal 10:", p10.nome, "-", p10.especie, "-", p10.idade)
