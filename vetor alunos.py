1794055ffb7a1fe7da04ecc46636a30be315a7b3
yamilaluno <alanocaflores8@gmail.com>
2025-10-06 10:10:02 -0300

    Add files via upload

---
+++ vetor alunos.py
@@ -0,0 +1,8 @@
+alunos = ["Diego", "Matheus", "Maria", "Rodrigo", "Luan", "Lucca", "Gabriel", "Yamil", "Ana", "Paulo", "Sofia", "Miguel", "Tati", "Lucas", "Felipe", "Paula", "Andre", "Camila", "Carol", "Davi", "Erike", "Sara", "Enzo", "Pedro", "Laura"]
+
+turma = input("Digite um nome do aluno: ")
+
+if turma in alunos:
+    print("esse aluno(a) tá na turma")
+else:
+    print("esse aluno(a) não esta na turma")
