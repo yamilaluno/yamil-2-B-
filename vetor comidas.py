8e8084e82d9b95992a543999c36242a4b0044ba2
yamilaluno <alanocaflores8@gmail.com>
2025-10-06 08:58:43 -0300

    Add files via upload

---
+++ vetor comidas.py
@@ -0,0 +1,11 @@
+saldaveis = ["alface", "cebola", "cenoura"]
+
+naosaldavel = ["pizza", "frango frito", "salsicha"]
+
+comidas = input("Digite a comida: ")
+
+if comidas in saldaveis:
+    print("Sim, é saldavel")
+
+if comidas in naosaldavel:
+    print("Não, é saldavel")
---
+++ vetor.py
@@ -0,0 +1,11 @@
+cores = ["vermelho", "verde", "roxo"]
+
+print("As cores são", cores)
+
+frutas = ["maçã", "pera", "uva", "pessego", "banana"]
+
+print("Primeira fruta: ", frutas[0])
+print("Ultima fruta: ", frutas[4])
+
+frutas[2] = "morango"
+print("Novo vetor: ", frutas)
