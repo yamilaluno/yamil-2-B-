e4262d3a963acfb1176e2c9386fc8f527adce2db
yamilaluno <alanocaflores8@gmail.com>
2025-10-06 09:19:02 -0300

    Add files via upload

---
+++ vetor filmes.py
@@ -0,0 +1,11 @@
+filmes = ["Os Caras Malvados 2", "Smurfs", "Gente Grande 2", "a fuga das galhinhas", "era do gelo 3"]
+
+print("Os filmes", filmes)
+
+meusfilmes = ["Os Caras Malvados 2", "Smurfs", "Gente Grande 2", "a fuga das galhinhas", "era do gelo 3"]
+
+print("Primeira fruta: ", filmes[0])
+print("Ultima fruta: ", filmes[4])
+
+meusfilmes[2] = "Frozen 2"
+print("Novo vetor: ", meusfilmes)
