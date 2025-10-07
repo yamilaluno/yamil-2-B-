601125204452c7e83bc91ceae482300742e6f83a
yamilaluno <alanocaflores8@gmail.com>
2025-10-07 08:13:12 -0300

    Add files via upload

---
+++ indice.py
@@ -0,0 +1,5 @@
+comidas = ["pizza", "sushi", "hambúguer", "açai", "lasanha", "hot-dog", "churrasco"]
+
+indice = int(input("digite um numero de 0 a 6: "))
+
+print("você escolheu: ", comidas[indice])
