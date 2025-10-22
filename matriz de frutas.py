99209550776ff2a848a2bff09978d437548d26a9
yamilaluno <alanocaflores8@gmail.com>
2025-10-22 08:52:35 -0300

    Add files via upload

---
+++ matriz de frutas.py
@@ -0,0 +1,21 @@
+frutas = [
+    ["maçã", "banana", "pera"],
+    ["abacate", "mamão", "abacaxi"],
+    ["manga", "melão", "melancia"],
+    ["uva", "caju", "jaca"],
+    ["caqui", "laranja", "mexerica"]
+]
+
+print(frutas[1][0])
+
+frutas[0][0] = "kiwi"
+print("Novo nome: ", frutas)
+
+frutas[0][1] = "pitaya"
+print("Novo nome: ", frutas)
+
+frutas[0][2] = "pessego"
+print("Novo nome: ", frutas)
+
+frutas[1][0] = "limão"
+print("Novo nome: ", frutas)
