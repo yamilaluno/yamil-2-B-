d00994dc7e83cd29ccc592b9183c15cd23d18a9a
yamilaluno <alanocaflores8@gmail.com>
2025-10-22 07:50:10 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,15 @@
+matriz = [
+    ["Andrew", "Thay", "Enzo"],
+    ["Evelyn", "Rodrigo", "Matheus"]
+]
+
+print(matriz[1][0])
+
+matriz[0][0] = "Gabriel"
+print("Novo nome: ", matriz)
+
+matriz[0][1] = "Diego"
+print("Novo nome: ", matriz)
+
+matriz[0][2] = "Lucca"
+print("Novo nome: ", matriz)
